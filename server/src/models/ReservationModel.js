da9797e3621945aedc9b043040c16802a07ed624
maria vasquez <maria.vasquez@developersinstitute.co.nz>
2023-05-15 06:11:09 +1200

    add schema for reservations

--- server/src/models/ReservationModel.js
+++ server/src/models/ReservationModel.js
@@ -1,1 +1,15 @@
 // FIXME export a model for Reservations
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const reservationModel = new Schema({
+  _id: { type: Object, required: true },
+  partySize: { type: Number, required: true },
+  date: { type: Date, required: true },
+  userId: { type: String, required: true },
+  restaurantName: { type: String, required: true },
+});
+
+const reservation = mongoose.model("Reservation", reservationModel);
+
+module.exports = reservation;
