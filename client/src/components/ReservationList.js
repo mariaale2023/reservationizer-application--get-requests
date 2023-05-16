f07a4ad08d9e54e16fe5406bf695b1dfd3e98da0
maria vasquez <maria.vasquez@developersinstitute.co.nz>
2023-05-16 12:56:11 +1200

    fomate of reservation list

--- client/src/components/ReservationList.js
+++ client/src/components/ReservationList.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import "./ReservationList.css";
 import { Link } from "react-router-dom";
 
@@ -5,6 +6,13 @@ const ReservationList = () => {
   return (
     <>
       <h1>Upcoming reservations</h1>
+      <ul>
+        <li>
+          <h2>Name Restaurant</h2>
+          <p>6:30am Fri 17 Nov, 2023</p>
+          <Link>View detail</Link>
+        </li>
+      </ul>
     </>
   );
 };
