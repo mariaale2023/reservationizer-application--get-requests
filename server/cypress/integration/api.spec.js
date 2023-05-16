2d9677646890bb505152ce9de3da2349712bfff9
maria vasquez <maria.vasquez@developersinstitute.co.nz>
2023-05-17 11:04:09 +1200

    chech test

--- server/cypress/integration/api.spec.js
+++ server/cypress/integration/api.spec.js
@@ -2,14 +2,14 @@ describe("API server", () => {
   it("GET /reservations returns correct response", () => {
     const expected = [
       {
-        id: "507f1f77bcf86cd799439011",
+        _id: "507f1f77bcf86cd799439011",
         partySize: 4,
         date: "2023-11-17T06:30:00.000Z",
         userId: "614abe145f317b89a2e36883",
         restaurantName: "Island Grill",
       },
       {
-        id: "614abf0a93e8e80ace792ac6",
+        _id: "614abf0a93e8e80ace792ac6",
         partySize: 2,
         date: "2023-12-03T07:00:00.000Z",
         userId: "614abe145f317b89a2e36883",
