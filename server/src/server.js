736d8ecb8b9ab3924f5ce475ff70b3e389e751a9
maria vasquez <maria.vasquez@developersinstitute.co.nz>
2023-05-21 15:07:33 +1200

    try to deploy on Render - add link in server

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,12 @@
+const cors = require("cors");
+
 const port = process.env.API_ENDPOINT || 5001;
 // const enpoint = process.env.API_ENDPOINT || 5001;
 const connectionURI =
   process.env.DATABASE_URI || "mongodb://localhost:27017/mongo";
 const app = require("./app");
+
+app.use(cors({ origin: ["https://reservationizer-app.onrender.com/"] }));
 
 const mongoose = require("mongoose");
 mongoose.connect(connectionURI);
